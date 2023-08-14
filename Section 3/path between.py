7455add6b90fda4f02dc402ae0edf376a4bf1ec0
fabinQ <maciejfabinski@gmail.com>
2023-08-14 19:22:14 +0200

    Sciezka grafu.

--- Section 3/path between.py
+++ Section 3/path between.py
@@ -8,24 +8,31 @@ V = {
     'H': ['P', 'Q', 'K']
 }
 
-def is_path_between(V, source, dest):
-    list_to_check = source
+def is_path_between(V,source, dest):
+    list_to_check = [source]
     list_visit = []
-    print('list_to_check ', list_to_check)
-    print('list_visit ', list_visit)
+    # print('list_to_check ', list_to_check)
+    # print('list_visit ', list_visit)
     while list_to_check:
         curent_node = list_to_check.pop(0)
-        print('\tcurent_node ',curent_node)
+        # print('\tlist_to_check pop ',list_to_check)
+        # print('\tlist_visit ', list_visit)
+        # print('\tcurent_node ',curent_node)
         if curent_node == dest:
+            print('BINGO!')
             return True
         else:
             if curent_node not in list_visit:
-                list_visit.appdend(curent_node)
-            for i in V[curent_node]:
-                if V[curent_node] not in list_to_check:
-                    list_to_check.append(i)
+                list_visit.append(curent_node)
+                list_to_check.extend(V[curent_node])
+            # print('\t\tlist_to_check ', list_to_check)
+            # print('\t\tlist_visit ', list_visit)
+            # print('\t\tcurent_node ', curent_node)
+    return False
 
 
-for n in V.keys():
-    for d in V.values():
-        is_path_between(V,d,'H')
+# for s in V.keys():
+#     for d in V.keys():
+#         if s != d:
+#             print(f' Path {s} - {d} = ', is_path_between(V, s, d))
+print(' Path E - H = ', is_path_between(V, 'E', 'H'))
