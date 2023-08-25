0e72b1a0e2d70b8a1ba29d84be8ec1c7d4f5109f
fabinQ <maciejfabinski@gmail.com>
2023-08-25 23:02:55 +0200

    tree polytree unfinished

---
+++ Section 3/LAB 20 Tree.py
@@ -0,0 +1,56 @@
+def get_root(graph):
+    the_root = None
+    # review all columns
+    for c in range(len(graph)):
+        # chek how many parents the node has
+        number_of_parents = 0
+        for r in range(len(graph)):
+            number_of_parents += graph[r][c]
+            # if the node hasn't parents, it is the root
+        if number_of_parents == 0:
+            the_root = c
+            break
+    # return found root or none
+    return the_root
+
+
+def is_tree(graph):
+     pass
+
+
+names = ['1 CEO', '2 Cognitive', '2 Systems', '2 Operations', '3 Healthcare', '3 Cloud', '3 Research', '3 Security', '3 Storage']
+structure1 = [
+    [0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+structure2 = [
+    [0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+structure3 = [
+    [0, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
