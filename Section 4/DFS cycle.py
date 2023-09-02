6e6365e987bf6f9161bb7b8f3165e19a530d99bd
fabinQ <maciejfabinski@gmail.com>
2023-09-02 23:43:12 +0200

    Depth First Search cycle

---
+++ Section 4/DFS cycle.py
@@ -0,0 +1,49 @@
+graph = [
+    [1, 4],
+    [2, 3, 4],
+    [3],
+    [],
+    [0, 5],
+    [5]
+]
+def has_cycle_to_node(graph, node, visited, path):
+
+    visited[node] = True
+    path.append(node)
+    print('node: ', node)
+    print(visited)
+
+    for neighbour in graph[node]:
+        print('neighbour ', neighbour)
+        print(visited[neighbour], '\n')
+        if not visited[neighbour]:
+            if has_cycle_to_node(graph, neighbour, visited, path):
+                return True
+        if neighbour in path:
+            cycle = path.copy()
+            while cycle[0] != neighbour:
+                del cycle[0]
+            cycle.append(neighbour)
+            print(cycle)
+            return True
+    del path[-1]
+    return False
+
+def has_cycle(graph):
+    visited = [False] * len(graph)
+    path = []
+
+    for node in range(len(graph)):
+        print('node: ',node)
+        print(visited[node])
+        if not visited[node]:
+            if has_cycle_to_node(graph, node, visited, path):
+                return True
+
+    return False
+
+if has_cycle(graph):
+    print('Graph has a cycle')
+else:
+    print('Graph has no cycle')
+# print(has_cycle(graph))
