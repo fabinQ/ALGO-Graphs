0a14c529ae0c0f8707711a512ba04de36f187ff6
fabinQ <maciejfabinski@gmail.com>
2023-09-07 22:34:59 +0200

    adding a comment

--- Section 4/Dijkstra.py
+++ Section 4/Dijkstra.py
@@ -6,12 +6,35 @@ graph = {
     'E': {}
 }
 
-'''Definiujemy listę kosztów i rodziców.'''
+'''
+Definiujemy listę kosztów i rodziców.
+Algorytm wygląda następująco:
+1. Wypełniamy listę wierzchołków inf, żeby waga każdego nieznanego wierzchołka była mniejsza.
+
+2. Stoimy na wierzchołku aktualnym wierzchołku (na początku początkowy) i sprawdzamy gdzie możemy iść. 
+Uzupełniamy tabele najtańszym kosztem.
+
+3. Dodajemy wierzchołek do przeprocesowanych.
+
+4. Teraz musimy znaleźć kolejny wierzchołek o najniższym koszcie oraz taki w którym nie byliśmy. 
+Przekazujemy koszt (szukamy najmniejszych) oraz przeprocesowane nody (nocy których nie byliśmy).
+
+5. Bierzemy listę kosztów (ten sam case co z tablica inf) porównujemy koszty aby znaleźć najtańszy node 
+oraz sprawdzamy czy juz tam byliśmy.
+
+6. Zwracamy jego nazwę i zaczynamy od początku z nowy nodem. 
+Wracamy do punktu 2, robimy tak długo aż (tak jak w tym przypadku dojdziemy do końca grafu.
+
+'''
 costs = {}
 parents = {}
 
 
 def get_cheapest_node(costs_dict, processed_list):
+    '''5. Z listy kosztów (cost_dic) musimy wybrać najtańszy node którego nie ma w przeprocesowanych.
+    Aby to zrobić musimy porównać z nieskończonością i schodzić do najniższego kosztu.
+    Nazwa najtańszego wierzchołka defoultowo jest None. W razie, gdyby był to ostatni wierzchołek
+    zwróci None i pętla while się zakończy.'''
     cheapest_cost = float('inf')
     cheapest_key = None
 
@@ -23,26 +46,28 @@ def get_cheapest_node(costs_dict, processed_list):
 
 
 for name in graph.keys():
-    '''Uzupełniamy tablice kosztów na infinity.'''
+    '''1.Uzupełniamy tablice kosztów na infinity.'''
     costs[name] = float('inf')
 
-'''Ustawiamy tablice odwiedzonych wierzchołków oraz wierzchołek startowy.'''
+'''1.Ustawiamy tablice odwiedzonych wierzchołków oraz wierzchołek startowy.'''
 processed = []
 current_node = 'A'
 costs[current_node] = 0
 parents[current_node] = None
 
-'''Wybieranie następnego wierzchołka w algorytmie: jest to taki wierzchołek do którego możemy się dostać nie był on 
+'''2.Wypełniamy koszt sąsiadów jeśli jest najmniejszy, czyli jest to taki wierzchołek do którego możemy się dostać nie był on 
 przetworzony oraz jest on najtańszy. Warunkiem kontrolnym jest current_node, czyli jeśli jest znany, jeśli nie jest tzn. 
 że skończyliśmy'''
 while current_node:
 
     for neighbour in graph[current_node]:
-        '''Aktualizujemy cenę tylko jeśli znaleźliśmy tańszą drogę. Dlatego inf na początku.'''
+        '''2.Sprawdzamy gdzie możemy isć i ewentualnie aktualizujemy cenę - tylko jeśli znaleźliśmy tańszą drogę. 
+        Dlatego inf na początku.'''
         if costs[neighbour] > costs[current_node] + graph[current_node][neighbour]:
             costs[neighbour] = costs[current_node] + graph[current_node][neighbour]
             parents[neighbour] = current_node
-    '''Gdy odwiedzimy wszystkie dzieci wierzchołka dodajemy go do odwiedzonych. Następnie ustalamy następny węzeł. '''
+    '''4. Gdy odwiedzimy wszystkie dzieci wierzchołka dodajemy go do odwiedzonych. 
+    Następnie ustalamy następny najtańszy węzeł (z sumy poprzednich węzełów). '''
     processed.append(current_node)
     current_node = get_cheapest_node(costs, processed)
 
