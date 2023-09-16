8ad4f87ed120ba0253c888aa12abbdd1622d4806
fabinQ <maciejfabinski@gmail.com>
2023-09-16 16:21:54 +0200

    Floyd-Warshall algo

--- Section 4/Floyd-Warshall.py
+++ Section 4/Floyd-Warshall.py
@@ -1,5 +1,4 @@
 names = ['A','B','C','D','E']
-names_index = list(map(lambda x: names.index(x), names))
 
 graph = [#   A  B  C  D  E
             [0, 2, 0, 4, 0], # A
@@ -10,7 +9,6 @@ graph = [#   A  B  C  D  E
 ]
 
 def print_matrix(matrix, names):
-    global N
     N = len(matrix)
     line = '    '
     for n in names:
@@ -23,10 +21,9 @@ def print_matrix(matrix, names):
         print(line)
 
 
-print_matrix(graph, names)
+N = len(graph)
 cost_matrix = [[float('inf') for x in range(N)] for x in range(N)]
 parent_matrix = [['' for x in range(N)] for x in range(N)]
-
 
 for i in range(N):
     for j in range(N):
@@ -37,17 +34,34 @@ for i in range(N):
             cost_matrix[i][j] = graph[i][j]
             parent_matrix[i][j] = i
 
+
 print()
 print_matrix(cost_matrix, names)
 print()
 print_matrix(parent_matrix, names)
 
-for node in names_index:
-    print()
-    print(node)
-    for relations in graph:
-        print(relations)
-        for i in relations:
-            print(i)
-            if node == graph.index(relations):
-                print('tu')
+for p in range(N):
+    for i in range(N):
+        for j in range(N):
+            if cost_matrix[i][j] > cost_matrix[i][p] + cost_matrix[p][j]:
+                cost_matrix[i][j] = cost_matrix[i][p] + cost_matrix[p][j]
+                parent_matrix[i][j] = parent_matrix[p][j]
+
+print()
+print_matrix(cost_matrix, names)
+print()
+print_matrix(parent_matrix, names)
+
+start_node = 0
+end_node = 4
+path = []
+
+while start_node != end_node:
+    end_node = parent_matrix[start_node][end_node]
+    path.append(end_node)
+path.reverse()
+path.append(4)
+
+start_node = 0
+end_node = 4
+print(f'From node {names[start_node]} to {names[end_node]} you must go through {"->".join(names[x] for x in path)}')
